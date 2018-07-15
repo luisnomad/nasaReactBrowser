44e749412c1222c2560e8928091ae654ccc5a8ae
Luis Serrano <luis@roostermoney.com>
2018-07-15 14:09:02 +0200

    Router and pages

---
+++ src/components/asset/Asset.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Search = () => (
+    <div>Search page</div>
+);
+
+export default Search;
