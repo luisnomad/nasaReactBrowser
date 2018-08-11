9239e1db762497ad4d5acc9ac67cc5cb842f59d7
Luis Serrano <luis@roostermoney.com>
2018-08-11 15:15:08 +0200

    Testing components with <Link />
    Added a technique that allows mounting components containing <Link />.

--- src/__tests__/components/Results.js
+++ src/__tests__/components/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Results, ResultsCard } from '../../components/search';
+import { MemoryRouter } from 'react-router';
 
 import mockData from '../../mockData/responseMock';
 import noResultsMock from '../../mockData/noResultsMock';
@@ -8,14 +9,17 @@ import noResultsMock from '../../mockData/noResultsMock';
 // NOTE: This cheatsheet is quite useful -> https://devhints.io/enzyme
 
 const getResultsCard = (item) => (
-    <ResultsCard
-        key={1}
-        index={0}
-        title={item.data[0].title}
-        description={item.data[0].description}
-        author={item.data[0].center}
-        image={item.data[0].media_type === 'image' ? item.links[0].href : ''}
-        mediaType={item.data[0].media_type} />
+    // Wrapping in <MemoryRouter /> allows testing components containing <Link /> 
+    mount(<MemoryRouter>
+        <ResultsCard
+            key={1}
+            index={0}
+            title={item.data[0].title}
+            description={item.data[0].description}
+            author={item.data[0].center}
+            image={item.data[0].media_type === 'image' ? item.links[0].href : ''}
+            mediaType={item.data[0].media_type} />
+    </MemoryRouter>).find('ResultsCard')
 );
 
 describe('<Results />', () => {
@@ -32,9 +36,11 @@ describe('<Results />', () => {
 
     it('With provided mock data, 1st element should be a video', () => {
         let wrapped; 
-        wrapped = shallow (getResultsCard(mockData.collection.items[0]));
-        expect(wrapped.hasClass('video')).toBeTruthy();
-        wrapped = shallow (getResultsCard(mockData.collection.items[7]));
-        expect(wrapped.hasClass('image')).toBeTruthy();
+        wrapped = getResultsCard(mockData.collection.items[0]);
+        console.log(wrapped.find('.videssso'));
+        expect(wrapped.find('.video')).toBeTruthy();
+        
+        wrapped = getResultsCard(mockData.collection.items[7]);
+        expect(wrapped.find('.image')).toBeTruthy();
     });
 });
