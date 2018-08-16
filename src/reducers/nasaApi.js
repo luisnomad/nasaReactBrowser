f0f380300adb98a8cd224b13c80454d3c9eb51d4
Luis Serrano <luis@roostermoney.com>
2018-08-16 15:29:16 +0200

    Picture of the day
    - Added action, reducer, container placeholder and test spec.

--- src/reducers/nasaApi.js
+++ src/reducers/nasaApi.js
@@ -2,6 +2,9 @@ import {
   FETCH_DATA_SUCCESS,
   FETCH_DATA_FAIL,
   FETCH_ASSET_SUCCESS,
+  FETCH_ASSET_FAIL,
+  FETCH_NASAPOTD_SUCCESS,
+  FETCH_NASAPOTD_FAIL,
   CLEAR_ASSET
 } from '../actions/types';
 
@@ -15,6 +18,21 @@ const nasaApi = (state = {}, action) => {
       return Object.assign({}, state, {
         error: null,
         assetContent: action.payload
+      });
+    case FETCH_ASSET_FAIL:
+      return Object.assign({}, state, {
+        error: action.error,
+        assetContent: null
+      });
+    case FETCH_NASAPOTD_SUCCESS:
+      return Object.assign({}, state, {
+        error: null,
+        NasaPictureOfTheDay: action.payload
+      });
+    case FETCH_NASAPOTD_FAIL:
+      return Object.assign({}, state, {
+        error: action.error,
+        NasaPictureOfTheDay: null
       });
     case CLEAR_ASSET:
       return Object.assign({}, state, {
